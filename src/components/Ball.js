41bdc32a8936c8619557a482d6069068a82d530a
Laureline Vaucoy <laureline.vaucoy@gmail.com>
2021-03-27 17:56:24 +0000

    Initial commit - set up folders

---
+++ src/components/Ball.js
@@ -0,0 +1,11 @@
+function Ball() {
+    
+    return(
+        <div className="ball">
+            <p>picture of Ball</p>
+        </div>
+    )
+
+}
+
+export default Ball;
---
+++ src/container/BallContainer.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import Ball from '../components/Ball'
+
+function BallContainer() {
+
+    return (
+        <div className="ball-container">
+            <h2>insert form question here</h2>
+            <Ball/>
+        </div>
+    );
+    
+}
+
+export default BallContainer;

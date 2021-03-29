53ebd0ad167a17ca92e2d5afefac09123b99a80c
Laureline Vaucoy <laureline.vaucoy@gmail.com>
2021-03-29 14:51:06 +0100

    Added random answers

--- src/container/BallContainer.js
+++ src/container/BallContainer.js
@@ -7,7 +7,31 @@ function BallContainer() {
     const [message, setMessage] = useState("8");
 
     const determineAnswer = (submittedQuestion) => {
-        setMessage(submittedQuestion);
+        let randomNumber = Math.round(Math.random()*7);
+        if (randomNumber = 0) {
+            setMessage('It is certain');
+          }
+          else if (randomNumber = 1) {
+            setMessage('It is decidedly so');
+          }
+          else if (randomNumber = 2) {
+              setMessage('Reply hazy try again');
+          }
+          else if (randomNumber = 3) {
+            setMessage('Cannot predict now');
+          }
+          else if (randomNumber = 4) {
+            setMessage('Do not count on it');
+          }
+          else if (randomNumber = 5) {
+              setMessage('My sources say no');
+          }
+          else if (randomNumber = 6) {
+              setMessage('Outlook not so good');
+          }
+          else if (randomNumber = 7) { 
+              setMessage('Signs point to yes');
+          }
     };
 
     return (
